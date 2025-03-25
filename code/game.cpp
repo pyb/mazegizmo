5b70276f584ab617c10951bb3d9003f32e8d4028
pyb <pyb@pyb.me.uk>
2025-03-25 15:46:50 -0400

    Maze file format

--- code/game.cpp
+++ code/game.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include "getbno055.h"
 
+#include <fstream>
+#include <string>
 using namespace std;
 
 //#define SENSOR true
@@ -15,7 +17,6 @@ int verbose = 0;
 
 const int screen_width = 800;
 const int screen_height = 480;
-//const float pixelsPerMeter = 1000.0f;
 const float ballDensity = 1.0f;
 const float wallLengthV = .5f;
 const float wallWidth = .020f;
@@ -30,7 +31,13 @@ const float pixelsPerMeter = screen_height / (wallLengthV);
 const float drawTransformAxisScale = .05f;
 const int sensorFramesSkipped = 10;
 
-// most of these are unused
+const float mazeX = -0.3f;
+const float mazeY = -0.2f;
+const float mazeWallLength = 0.034f;
+const float mazeWallThickness = 0.01f;
+const float kbdGravityStep = 0.1f;
+
+// Sensor stuff. most of these are unused
 int outflag = 0;
 int argflag = 0; // 1 dump, 2 reset, 3 load calib, 4 write calib
 char opr_mode[9] = {0};
@@ -38,6 +45,52 @@ char pwr_mode[8] = {0};
 char datatype[256];
 char senaddr[256] = "0x28";
 char i2c_bus[256] = I2CBUS;
+
+const int maze_width = 19;
+const int maze_height = 13;
+
+const int EMPTY = 0;
+const int VERTWALL = 1;
+const int HORWALL = 2;
+
+int maze[maze_width][maze_height];
+
+void initMaze()
+{
+	 int j = 0;
+	 
+	 std::ifstream infile("maze.txt");
+
+	 std::string line;
+	 while (std::getline(infile, line))
+	 {
+//		  cout << line << endl;
+		  for (int i = 0 ; i < maze_width; i++)
+		  {
+			   char c = line.at(i);
+			   int val;
+			   switch(c)
+			   {
+			   case '.':
+					val = EMPTY;
+					break;
+			   case 'I':
+					val = VERTWALL;
+					break;
+			   case '-':
+					val = HORWALL;
+					break;
+			   default:
+					val = EMPTY;
+					break;
+			   }
+			   maze[i][j] = val;
+		  }
+		  j++;
+	 }
+	 //maze[20][20] = 1;
+	 //exit(0);
+}
 
 void initSensor()
 {
@@ -55,26 +108,9 @@ b2Vec2 readGravity()
 		  printf("Error: Cannot read gravity vector data.\n");
 		  exit(-1);
 	 }
-	 printf("GRA %3.2f %3.2f %3.2f\n", (float)bnod.gravityx, (float)bnod.gravityy, bnod.gravityz);
+//	 printf("GRA %3.2f %3.2f %3.2f\n", (float)bnod.gravityx, (float)bnod.gravityy, bnod.gravityz);
 	 return (b2Vec2){(float)bnod.gravityy, (float)bnod.gravityx}; // axes are inverted rn
 }
-
-/*
-// Disabled for now
-
-void DrawEntity(const Entity* entity)
-{
-	// The boxes were created centered on the bodies, but raylib draws textures starting at the top left corner.
-	// b2Body_GetWorldPoint gets the top left corner of the box accounting for rotation.
-	b2Vec2 p = b2Body_GetWorldPoint(entity->bodyId, (b2Vec2) { -entity->extent.x, -entity->extent.y });
-	b2Rot rotation = b2Body_GetRotation(entity->bodyId);
-	float radians = b2Rot_GetAngle(rotation);
-
-	Vector2 ps = {p.x, p.y};
-	DrawTextureEx(entity->texture, ps, RAD2DEG * radians, 1.0f, WHITE);
-
-}
-*/
 
 b2DebugDraw m_debugDraw;
 
@@ -211,8 +247,51 @@ void initDebug() {
 
 }
 
+void buildMaze(b2WorldId worldId, float wallLength, float wallThickness, float startX, float startY)
+{
+	 b2BodyDef wallBodyDef = b2DefaultBodyDef();
+	 b2ShapeDef wallShapeDef = b2DefaultShapeDef();
+
+	 b2Vec2 wallHExtent = { wallLength/2, wallThickness/2};
+	 b2Polygon wallH = b2MakeBox(wallHExtent.x, wallHExtent.y);
+	 b2Polygon wallV = b2MakeBox(wallHExtent.y, wallHExtent.x);
+
+	 for (int i = 0 ; i < maze_width ; i++)
+	 {
+		  for (int j = 0 ; j < maze_height ; j++)
+		  {
+			   if (maze[i][j] == EMPTY)
+					continue;
+
+			   wallBodyDef = b2DefaultBodyDef();
+			   wallBodyDef.position = (b2Vec2){startX + i * wallLength, startY + j * wallLength};
+			   wallBodyDef.enableSleep = false;
+			   wallBodyDef.type = b2_staticBody;
+
+			   wallShapeDef = b2DefaultShapeDef();
+			   
+			   b2BodyId wallId = b2CreateBody(worldId, &wallBodyDef);
+
+			   if (maze[i][j] == HORWALL)
+			   {
+					b2CreatePolygonShape(wallId, &wallShapeDef, &wallH);
+			   }
+			   else if (maze[i][j] == VERTWALL)
+			   {
+					b2CreatePolygonShape(wallId, &wallShapeDef, &wallV);
+			   }
+			   else
+			   {
+					cout << "Error in maze construction!" << endl;
+					exit(1);
+			   }
+		  }
+	 }
+}
+
 int main(void)
 {
+	 initMaze();
 #ifdef SENSOR
 	 initSensor();
 #endif
@@ -225,9 +304,6 @@ int main(void)
 	 camera.zoom = pixelsPerMeter;
 	
 	 SetTargetFPS(60);
-	
-//	 float lengthUnitsPerMeter = 10.0f;
-//	 b2SetLengthUnitsPerMeter(lengthUnitsPerMeter);
 
 	 initDebug();
 
@@ -285,6 +361,8 @@ int main(void)
 	 circle.radius = ballRadius;
 	 b2CreateCircleShape(ballBodyId, &ballShapeDef, &circle);
 
+	 buildMaze(worldId, mazeWallLength, mazeWallThickness, mazeX, mazeY);
+	 
 	 bool pause = false;
 
 	 int k = 0;
@@ -292,22 +370,22 @@ int main(void)
 	 {
 		  if (IsKeyDown(KEY_UP))
 		  {
-			   gravity.y -= 1.3f;
+			   gravity.y -= kbdGravityStep;
 			   b2World_SetGravity (worldId, gravity);
 		  }
 		  else if (IsKeyDown(KEY_DOWN))
 		  {
-			   gravity.y += 1.3f;
+			   gravity.y += kbdGravityStep;
 			   b2World_SetGravity (worldId, gravity);
 		  }
 		  else if (IsKeyDown(KEY_LEFT))
 		  {
-			   gravity.x -= 1.3f;
+			   gravity.x -= kbdGravityStep;
 			   b2World_SetGravity (worldId, gravity);
 		  }
 		  else if (IsKeyDown(KEY_RIGHT))
 		  {
-			   gravity.x += 1.3f;
+			   gravity.x += kbdGravityStep;
 			   b2World_SetGravity (worldId, gravity);
 		  }
 #ifdef SENSOR
